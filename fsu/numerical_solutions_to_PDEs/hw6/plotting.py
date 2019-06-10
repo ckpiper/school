32dff7f6efe0152b20450c97e68691bf88075953
Colton Piper <pipercolton@gmail.com>
2019-06-10 00:07:56 -0400

    Update plotting.py

--- fsu/numerical_solutions_to_PDEs/hw6/plotting.py
+++ fsu/numerical_solutions_to_PDEs/hw6/plotting.py
@@ -1,102 +1,117 @@
+# This plots the algorithms run in hw1.cpp 
 
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-pi = math.pi
+def main():
 
-a = 1.0;
-cells = 25;
-L = 1.0;
-h = L/cells;
-k = h/a * 0.25;
+	fileName = ["data/exact.txt", "data/constant.txt", "data/linear.txt", "data/minmod.txt"]
 
-# lam = np.asarray([0.2, 0.4, 0.5, 0.6, 0.8])
-lam = np.asarray([0.25, 0.5, 0.75])
-i = lam.size
+	a = 1;
+	cells =25;
+	L = 1.0;
+	h = L/cells;
+	k = h/a * 0.25
+	T = 100*k
+	n = 4
 
-MPlot = 300
-theta = np.asarray([(2*z/MPlot - 1)*pi for z in range(0,MPlot+1)])
-gUpwind = np.zeros([i,301])
-gOther = np.zeros([i,301])
-alphaUpwind = np.zeros([i,301])
-alphaOther = np.zeros([i,301])
+	# Round it to nearest integer as it should be an exact division add 1 for initial
+	# int = floor function brings N down then add 1 for initial then add one in  setZeros function for final
+	if(abs(round(T/k) - T/k) < 0.00000001):
+		N = round(T/k) + 1
+		final = 0
+		vv = np.zeros([4, N, cells])
+		print("Epsilon")
+	else:
+		N = int(T/k) + 1
+		final = 1
+		vv = np.zeros([4, N+1, cells])
+		print("Not-Epsilon")
 
 
-for n in range(0,i):
-	gUpwind[n] = np.sqrt(1 - 2*a*lam[n]*(1-a*lam[n])*(1 - np.cos(theta)))
-	gOther[n] = np.sqrt(    ( 1-a*lam[n]*(1-np.cos(theta)-0.5*(1-a*lam[n])*(np.sin(theta))**2) )**2   +   ( a*lam[n]*np.sin(theta)*(-1-0.5*(1-a*lam[n])*(1-np.cos(theta))) )**2     )
-	alphaUpwind[n] = -1*np.arctan( (-1*a*lam[n]*np.sin(theta)) / (1 - a*lam[n]*(1-np.cos(theta)))) / (a*theta*lam[n])
-	alphaOther[n] = -1*np.arctan(    ( a*lam[n]*np.sin(theta)*(-1-0.5*(1-a*lam[n])*(1-np.cos(theta))) ) / ( 1-a*lam[n]*(1-np.cos(theta)-0.5*(1-a*lam[n])*(np.sin(theta))**2) )     )  / (a*lam[n]*theta)
+	for m in range(0,n):
+		f = open(fileName[m], "r")
+		i = 0
+		for line in f:
+			row = np.asarray(line.split(" "))
+			row = np.delete(row, -1)					# Gets rid of the newline character
+			vv[m, i] = row
+			i = i + 1
 
-	
-# plt.plot(theta, gUpwind[0], label="Upwind " + str(lam[0]))
-# plt.plot(theta, gUpwind[1], label="Upwind " + str(lam[1]))
-# plt.plot(theta, gUpwind[2], label="Upwind " + str(lam[2]))
-# plt.plot(theta, gUpwind[3], label="Upwind " + str(lam[3]))
-# plt.plot(theta, gUpwind[4], label="Upwind " + str(lam[4]))
-# # plt.legend()
-# # plt.show()
 
-# plt.plot(theta, gOther[0], label="Other " + str(lam[0]))
-# plt.plot(theta, gOther[1], label="Other " + str(lam[1]))
-# plt.plot(theta, gOther[2], label="Other " + str(lam[2]))
-# plt.plot(theta, gOther[3], label="Other " + str(lam[3]))
-# plt.plot(theta, gOther[4], label="Other " + str(lam[4]))
+	title = ["Exact Solution", "Constant Approx", "Linear Approx", "Minmod Linear Approx"]
+	plotFile = ["plots/exact.eps", "plots/constant.eps", "plots/linear.eps", "plots/minmod.eps", ]
 
-# plt.title("|G(Theta)|" + ", a*lam = " + str(lam))
-# plt.legend()
-# plt.savefig("plots/gTheta.eps", format='eps')
-# plt.show()
+	xx = np.zeros([cells])
+	for j in range(0,cells):
+		xx[j] = h/2.0 + j*h
 
-# plt.plot(theta, alphaUpwind[0], label="Upwind " + str(lam[0]))
-# plt.plot(theta, alphaUpwind[1], label="Upwind " + str(lam[1]))
-# plt.plot(theta, alphaUpwind[2], label="Upwind " + str(lam[2]))
-# plt.plot(theta, alphaUpwind[3], label="Upwind " + str(lam[3]))
-# plt.plot(theta, alphaUpwind[4], label="Upwind " + str(lam[4]))
-# # plt.legend()
-# # plt.show()
+	if(final==0):
+		plt.plot(xx, vv[0][N-1], label="Exact")
+		plt.plot(xx, vv[1][N-1], label="Constant")
+		plt.plot(xx, vv[2][N-1], label="Linear")
+		plt.plot(xx, vv[3][N-1], label="Minmod")
+	else:
+		plt.plot(xx, vv[0][N], label="Exact")
+		plt.plot(xx, vv[1][N], label="Constant")
+		plt.plot(xx, vv[2][N], label="Linear")
+		plt.plot(xx, vv[3][N], label="Minmod")
 
-# plt.plot(theta, alphaOther[0], label="Other " + str(lam[0]))
-# plt.plot(theta, alphaOther[1], label="Other " + str(lam[1]))
-# plt.plot(theta, alphaOther[2], label="Other " + str(lam[2]))
-# plt.plot(theta, alphaOther[3], label="Other " + str(lam[3]))
-# plt.plot(theta, alphaOther[4], label="Other " + str(lam[4]))
+	plt.legend()
+	plt.tick_params(labelbottom=True, labeltop=False, labelleft=True, labelright=True, bottom=True, top=True, left=True, right=True)
+	plt.title("Final Time = " + str(T) + " for Solutions")
+	plt.savefig("plots/finalTime.eps", format='eps')
+	plt.show()
 
-# plt.title("Alpha/a" + ", a*lam = " + str(a*lam))
-# plt.legend()
-# plt.savefig("plots/alpha.eps", format='eps')
-# plt.show()
 
-################333333333######33333333333333333333333333333333333333333333333333############3
+	# mult = 1
+	# plt.plot(xx, vv[0][0], label="Exact")
+	# plt.plot(xx, vv[0][mult], label="Exact")
+	# plt.plot(xx, vv[1][mult], label="Constant")
+	# plt.plot(xx, vv[2][mult], label="Linear")
+	# plt.plot(xx, vv[3][mult], label="Minmod")
 
-plt.plot(theta, gUpwind[0], label="Upwind " + str(lam[0]))
-plt.plot(theta, gUpwind[1], label="Upwind " + str(lam[1]))
-plt.plot(theta, gUpwind[2], label="Upwind " + str(lam[2]))
-# plt.legend()
-# plt.show()
+	# plt.legend()
+	# plt.tick_params(labelbottom=True, labeltop=False, labelleft=True, labelright=True, bottom=True, top=True, left=True, right=True)
+	# plt.title("Time = " + str(k*mult) + " for Solutions")
+	# plt.savefig("plots/fTime" + str(mult) + ".eps", format='eps')
+	# plt.show()
 
-plt.plot(theta, gOther[0], label="Other " + str(lam[0]))
-plt.plot(theta, gOther[1], label="Other " + str(lam[1]))
-plt.plot(theta, gOther[2], label="Other " + str(lam[2]))
+	t1 = int(N/100)
+	t2 = int(N/33)
+	t3 = int(N/10)
+	t4 = int(N/3)
+	t5 = int(2*N/3)
+	t6 = int(5*N/6)
 
-plt.title("|G(Theta)|" + ", a*lam = " + str(lam))
-plt.legend()
-plt.savefig("plots/gTheta.eps", format='eps')
-plt.show()
 
-plt.plot(theta, alphaUpwind[0], label="Upwind " + str(lam[0]))
-plt.plot(theta, alphaUpwind[1], label="Upwind " + str(lam[1]))
-plt.plot(theta, alphaUpwind[2], label="Upwind " + str(lam[2]))
-# plt.legend()
-# plt.show()
+	for j in range(0,n):
+		plt.plot(xx, vv[j][0], label="t=0")
+		# plt.plot(xx, vv[j][1], label="t=" + str(k*1))
+		# plt.plot(xx, vv[j][2], label="t=" + str(k*2))
+		# plt.plot(xx, vv[j][t1], label="t=" + str(k*t1))
+		plt.plot(xx, vv[j][t2], label="t=" + str(k*t2))
+		plt.plot(xx, vv[j][t3], label="t=" + str(k*t3))
+		plt.plot(xx, vv[j][t4], label="t=" + str(k*t4))
+		plt.plot(xx, vv[j][t5], label="t=" + str(k*t5))
+		plt.plot(xx, vv[j][t6], label="t=" + str(k*t6))
+		if(final==0):
+			plt.plot(xx, vv[j][N-1], label="Final=" + str(T))
+		else:
+			plt.plot(xx, vv[j][N], label="Final=" + str(T))
+		plt.legend()
+		plt.tick_params(labelbottom=True, labeltop=False, labelleft=True, labelright=True, bottom=True, top=True, left=True, right=True)
+		plt.title(title[j])
+		plt.savefig(plotFile[j], format='eps')
+		plt.show()
 
-plt.plot(theta, alphaOther[0], label="Other " + str(lam[0]))
-plt.plot(theta, alphaOther[1], label="Other " + str(lam[1]))
-plt.plot(theta, alphaOther[2], label="Other " + str(lam[2]))
 
-plt.title("Alpha/a" + ", a*lam = " + str(a*lam))
-plt.legend()
-plt.savefig("plots/alpha.eps", format='eps')
-plt.show()
 
+
+
+
+
+
+
+main()
